e20e856b96e6e7740f5fbfc8864e3a401339c4eb
hxxejx <uiu1442@naver.com>
2022-05-22 00:28:34 +0900

    edit main notice

--- vue-board-housedeal-todolist-login/src/components/home/NoticeList.vue
+++ vue-board-housedeal-todolist-login/src/components/home/NoticeList.vue
@@ -1,17 +1,12 @@
 <template>
   <b-container class="bv-example-row mt-3">
     <b-row>
-      <b-col>
-        <b-alert show><h3>NOTICE</h3></b-alert>
-      </b-col>
-    </b-row>
-    <b-row>
       <b-col v-if="articles.length">
         <b-table-simple hover responsive>
-          <b-thead head-variant="dark">
+          <b-thead head-variant="white">
             <b-tr>
               <b-th>글번호</b-th>
-              <b-th>제목</b-th>
+              <b-th>공지사항</b-th>
               <b-th>작성일</b-th>
             </b-tr>
           </b-thead>
@@ -33,33 +28,19 @@ import { mapState, mapActions } from "vuex";
 import NoticeListItem from "@/components/home/item/NoticeListItem";
 
 const noticeStore = "noticeStore";
-const memberStore = "memberStore";
 export default {
   name: "NoticeList",
   components: {
     NoticeListItem,
   },
-  data() {
-    return {
-      isAdmin: false,
-    };
-  },
   created() {
     this.getArticleList();
-
-    if (this.userInfo.userid === "admin") {
-      this.isAdmin = true;
-    }
   },
   computed: {
     ...mapState(noticeStore, ["articles"]),
-    ...mapState(memberStore, ["userInfo"]),
   },
   methods: {
     ...mapActions(noticeStore, ["getArticleList"]),
-    moveWrite() {
-      this.$router.push({ name: "noticeRegister" });
-    },
   },
 };
 </script>
