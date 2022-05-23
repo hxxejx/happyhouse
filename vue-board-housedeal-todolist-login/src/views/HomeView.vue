f7847ff27af075b704a0e9f76810f57d8b403efc
hxxejx <uiu1442@naver.com>
2022-05-23 13:21:18 +0900

    Update HomeView.vue

--- vue-board-housedeal-todolist-login/src/views/HomeView.vue
+++ vue-board-housedeal-todolist-login/src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <b-container class="bv-example-row mt-3 text-center">
-    <!-- <h3 class="underline-steelblue"><b-icon icon="house"></b-icon> SSAFY</h3> -->
     <b-row>
       <b-col>
         <b-carousel
@@ -16,16 +15,16 @@
             text="해피하우스 서비스를 찾아주셔서 감사합니다."
             img-src="https://picsum.photos/1024/480/?image=10"
           ></b-carousel-slide>
-          <b-carousel-slide
-            caption="Welcome! Happy House"
-            text="해피하우스 서비스를 찾아주셔서 감사합니다."
-            img-src="https://picsum.photos/1024/480/?image=12"
-          ></b-carousel-slide>
-          <b-carousel-slide
-            caption="Welcome! Happy House"
-            text="해피하우스 서비스를 찾아주셔서 감사합니다."
-            img-src="https://picsum.photos/1024/480/?image=22"
-          ></b-carousel-slide>
+          <a href="https://housing.seoul.go.kr/site/main/content/sh01_040081">
+            <b-carousel-slide
+              img-src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbaTWNQ%2FbtrCS1A39ia%2FuOY72dV9sg8afvvqJbgGEK%2Fimg.png"
+            ></b-carousel-slide>
+          </a>
+          <a href="https://seoul.childcare.go.kr/ccef/main.jsp">
+            <b-carousel-slide
+              img-src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FpTyIq%2FbtrCJcX96n7%2FuknuM0BKSBAQ66eB59sK80%2Fimg.png"
+            ></b-carousel-slide>
+          </a>
         </b-carousel>
       </b-col>
     </b-row>
@@ -34,44 +33,18 @@
     </b-row>
     <b-row>
       <b-col>
-        <notice-list></notice-list>
-        <!-- <b-jumbotron
-          bg-variant="muted"
-          text-variant="dark"
-          border-variant="dark"
-        >
-          <template #header><b>영역3</b></template>
-
-          <template #lead> <br /> </template>
-
-          <hr class="my-4" />
-
-          <p></p>
-        </b-jumbotron> -->
+        <house-deal-list></house-deal-list>
       </b-col>
       <b-col>
         <house-list></house-list>
-        <!-- <b-jumbotron
-          bg-variant="muted"
-          text-variant="dark"
-          border-variant="dark"
-        >
-          <template #header><b>영역3</b></template>
-
-          <template #lead> <br /> </template>
-
-          <hr class="my-4" />
-
-          <p></p>
-        </b-jumbotron> -->
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import NoticeList from "@/components/home/NoticeList.vue";
 import HouseList from "@/components/home/HouseList.vue";
+import HouseDealList from "@/components/home/HouseDealList.vue";
 
 export default {
   name: "HomeView",
@@ -79,8 +52,14 @@ export default {
     msg: String,
   },
   components: {
-    NoticeList,
     HouseList,
+    HouseDealList,
+  },
+  methods: {
+    gotoSite() {
+      window.location.href =
+        "https://housing.seoul.go.kr/site/main/content/sh01_040081";
+    },
   },
 };
 </script>
