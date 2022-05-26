d1e7f3431fc634fdbdb65a41844054941d7dfb43
hxxejx <uiu1442@naver.com>
2022-05-26 20:02:22 +0900

    dist

--- happyhouse_backend/src/main/java/com/ssafy/vue/config/WebConfig.java
+++ happyhouse_backend/src/main/java/com/ssafy/vue/config/WebConfig.java
@@ -46,4 +46,18 @@ public class WebConfig implements WebMvcConfigurer {
 //        registry.addResourceHandler("/swagger-ui/index.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui/index.html");
 //        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/**/*")
+							.addResourceLocations("classpath:/static/")
+							.resourceChain(true)
+							.addResolver(new PathResourceResolver() {
+									@Override
+									protected Resource getResource(String resourcePath, Resource location) throws IOException {
+											Resource requestedResource = location.createRelative(resourcePath);
+											return requestedResource.exists() && requestedResource.isReadable() ? requestedResource : new ClassPathResource("/static/index.html");
+									}
+							});
+	}
 }
